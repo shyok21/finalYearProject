3001621861d03fe7303efc8c1c557ceb966253ab
shyok21 <shyokmutsuddi21@gmail.com>
2020-12-25 11:59:19 +0530

    mysql connection added

--- students/index.js
+++ students/index.js
@@ -3,116 +3,55 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const util = require('util');
+const mysql = require('mysql');
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const con = mysql.createConnection({
+    host: "sql12.freemysqlhosting.net",
+    user: "sql12383722",
+    password: "KB3LCenv9V",
+    database: "sql12383722"
+});
+console.log('Connection Established');
 app.use(express.static(__dirname + '/public'));
 var htmlFile = fs.readFileSync("./index.html", "utf-8");
 var htmlFileSend = htmlFile.replace("{%Login Error%}", "");
-var x = fs.readFileSync('../tempDatabase/Student.txt', 'utf-8');
-var students = x.split("\n");
-var tempStudent = [];
-for (var i = 0; i < students.length; i++) {
-    var t = students[i].split(",");
-    tempStudent.push(t);
-}
-var tempPRC = [
-    ['shyok.m', 'shyokju', 'Shyok Mutsuddi', 'CE'],
-    ['shubham.g', 'shubhamju', 'Shubham Goel', 'ME'],
-    ['abhishek.d', 'abhishekju', 'Abhishek De', 'CSE'],
-    ['roopkatha.s', 'roopkathaju', 'Roopkatha Samanta', 'EE']
-];
-var tempDC = ['abhishek.g', 'abhishekju', 'Abhishek Goel'];
-isExist = (usr, psw) => {
-    for (var i = 0; i < tempStudent.length; i++) {
-        if (usr === tempStudent[i][0] && psw === tempStudent[i][1])
-            return i;
-    }
-    return -1;
-}
-isExistDC = (usr, psw) => {
-    if (usr === tempDC[0] && psw === tempDC[1])
-        return 1;
-    return 0;
-}
-getDept = (usr, psw) => {
-    for (var i = 0; i < tempPRC.length; i++) {
-        if (usr === tempPRC[i][0] && psw === tempPRC[i][1])
-            return tempPRC[i][3];
-    }
-    return -1;
-}
-getStudents = (dept) => {
-
-    var str = "";
-    for (var i = 0; i < tempStudent.length; i++) {
-        if (tempStudent[i][5] == 0) {
-            if (tempStudent[i][3] === dept)
-                str = str + tempStudent[i][2] + "&nbsp;&nbsp;" + tempStudent[i][4] + "<br>";
+con.connect(function(err) {
+    if (!(err))
+        console.log("Connected to Database");
+    app.get("/", (req, res) => {
+        res.send(htmlFileSend);
+    });
+    app.post("/login", urlencodedParser, (req, res) => {
+        console.log(req.body);
+        var isEmpty = 0;
+        if (req.body.username == '' || req.body.password == '') {
+            var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9888; Username & Password Can't be Empty!");
+            var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-invalid");
+            isEmpty = 1;
+            res.send(htmlNewFile);
         }
-    }
-    return str;
-}
-getStudentDC = (phase) => {
-    var str = "";
-    for (var i = 0; i < tempStudent.length; i++) {
-        if (phase == tempStudent[i][5]) {
-            str = str + tempStudent[i][2] + "&nbsp;&nbsp;" + tempStudent[i][4] + "<br>";
-        }
-    }
-    return str;
-}
-app.get("/", (req, res) => {
-    res.send(htmlFileSend);
-});
-app.post("/login", urlencodedParser, (req, res) => {
-    console.log(req.body);
-    var isEmpty = 0;
-    if (req.body.username == '' || req.body.password == '') {
-        var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9888; Username & Password Can't be Empty!");
-        var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-invalid");
-        isEmpty = 1;
-        res.send(htmlNewFile);
-    }
-    if (isEmpty == 0) {
-        var log = req.body.logintype;
-        if (log === 'Student') {
-            var flag = isExist(req.body.username, req.body.password);
-            if (flag == -1) {
-                var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9746; Invalid Username or Password!");
-                var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-cross");
-                res.send(htmlNewFile);
-            } else {
-                res.send('Hello');
-            }
-        } else if (log === 'PRC') {
-            var dept = getDept(req.body.username, req.body.password);
-            if (dept == -1) {
-                var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9746; Invalid Username or Password!");
-                var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-cross");
-                res.send(htmlNewFile);
-            } else {
-                console.log(dept);
-                var str = getStudents(dept);
-                res.send(str);
-            }
+        if (isEmpty == 0) {
+            var log = req.body.logintype;
+            var usr = req.body.username;
+            var psw = req.body.password;
+            var qry = util.format("select* from login where email='%s' and password='%s' and type='%s'", usr, psw, log);
+            con.query(qry, (err, result, fields) => {
+                if (err) throw err;
+                if (result.length == 0) {
+                    var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9746; Invalid Username or Password!");
+                    var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-cross");
+                    res.send(htmlNewFile);
+                } else
+                    res.send(result);
+            });
 
-        } else if (log === 'DC') {
-            var flag = isExistDC(req.body.username, req.body.password);
-            if (flag == 0) {
-                var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9746; Invalid Username or Password!");
-                var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-cross");
-                res.send(htmlNewFile);
-            } else {
-                var str = getStudentDC(1);
-                res.send(str);
-            }
-        } else {
-            res.send('Link Broken');
         }
-    }
 
-});
-var port = 8000;
-app.listen(port, () => {
-    console.log("Server Created!");
-    console.log("https://localhost:" + port + "/");
+    });
+    var port = 8000;
+    app.listen(port, () => {
+        console.log("Server Created!");
+        console.log("https://localhost:" + port + "/");
+    });
 });
--- tempDatabase/index.js
+++ tempDatabase/index.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
-
+var express = require('express');
+var app = express();
 var con = mysql.createConnection({
   host: "sql12.freemysqlhosting.net",
   user: "sql12383722",
@@ -8,17 +9,18 @@ var con = mysql.createConnection({
 });
 var x;
 con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM student", function (err, result, fields) {
+  app.get("/",(req,res)=>{
+  var x = [];
+  con.query("SELECT name FROM student", function (err, result, fields) {
     if (err) throw err;
-    console.log(result[0].address);
-    x = result;
+    //x.push(result[0]);
+    //res.send(result);
   });
+  con.query("SELECT distinct* FROM login;", function (err, result, fields) {
+    if (err) throw err;
+    x.push(result);
+    res.send(x);
+  });
+  });
+  app.listen(3000,()=>{console.log("created")});
 });
-try{
-console.log("new:"+x[0]);
-}
-catch{
- console.log("Hello");
- }
- 
