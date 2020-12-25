2115d7d3c137126f2f3df11441ea89e784eb0f02
shyok21 <shyokmutsuddi21@gmail.com>
2020-12-25 14:23:32 +0530

    Database Connected for Student Login

--- students/index.js
+++ students/index.js
@@ -42,8 +42,43 @@ con.connect(function(err) {
                     var htmlNewFile = htmlFile.replace("{%Login Error%}", "&#9746; Invalid Username or Password!");
                     var htmlNewFile = htmlNewFile.replace("{%error-type%}", "login-cross");
                     res.send(htmlNewFile);
-                } else
-                    res.send(result);
+                } else {
+                    if (log === 'STUD') {
+                        var studentFile = fs.readFileSync("./student.html", "utf-8");
+                        var stud_id = result[0].id;
+                        var qrys = util.format("select name,registration_phase from student where stud_id='%s'", stud_id);
+                        con.query(qrys, (err, results, fields) => {
+                            var studentName = results[0].name;
+                            var studentPhase = results[0].registration_phase;
+                            var studentMainHtmlFile = htmlNewFile;
+                            if (studentPhase == 0) {
+                                var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
+                                var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your Application Gets Rejected.");
+                                for (var i = 0; i < 5; i++)
+                                    htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-rejected")
+                                res.send(htmlNewFile);
+                            } else {
+                                var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
+                                var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your registration is in Under Process. We will notify you once your Registration gets Complete. You can track your Registration Phase here.");
+                                for (var i = 0; i < 5; i++) {
+                                    if (studentPhase > i)
+                                        htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-accepted");
+                                    else
+                                        htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-process");
+                                }
+                                res.send(htmlNewFile);
+                            }
+                        });
+                    } else if (log === 'SUP') {
+                        res.send("Hello Supervisor " + result.name);
+                    } else if (log === 'RAC') {
+                        res.send("Hello RAC " + result.name);
+                    } else if (log === 'DC') {
+                        res.send('Doctorate Committie ' + result.name);
+                    } else {
+                        res.send('Hello PRC ' + result.name);
+                    }
+                }
             });
 
         }
