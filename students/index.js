f87a4c02c1ae73de1f939abf83055e0fc82d0277
shyok21 <shyokmutsuddi21@gmail.com>
2021-01-02 22:00:35 +0530

    Create Account Added

--- students/index.js
+++ students/index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const util = require('util');
 const mysql = require('mysql');
+var randomstring = require("randomstring");
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const con = mysql.createConnection({
     host: "sql12.freemysqlhosting.net",
@@ -25,6 +26,21 @@ con.connect(function(err) {
     });
     app.get("/newUser", (req, res) => {
         res.send(htmlRegisterFile);
+    });
+    var captcha;
+    app.get("/newAccount", (req, res) => {
+        var htmlFile = fs.readFileSync("./newAccount.html", "utf-8");
+        var captchaStr = randomstring.generate(6);
+        captcha = captchaStr;
+        var newHtmlFile = htmlFile;
+        for (var i = 0; i < captchaStr.length; i++) {
+            newHtmlFile = newHtmlFile.replace("{%captcha%}", captchaStr[i]);
+        }
+        // console.log(newHtmlFile);
+        res.send(newHtmlFile);
+    });
+    app.post("/validate", (req, res) => {
+        res.send(captcha);
     });
     app.post("/login", urlencodedParser, (req, res) => {
         console.log(req.body);
@@ -52,25 +68,30 @@ con.connect(function(err) {
                         var stud_id = result[0].id;
                         var qrys = util.format("select name,registration_phase from student where stud_id='%s'", stud_id);
                         con.query(qrys, (err, results, fields) => {
-                            var studentName = results[0].name;
-                            var studentPhase = results[0].registration_phase;
-                            var studentMainHtmlFile = htmlNewFile;
-                            if (studentPhase == 0) {
-                                var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
-                                var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your Application Gets Rejected.");
-                                for (var i = 0; i < 5; i++)
-                                    htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-rejected")
-                                res.send(htmlNewFile);
-                            } else {
-                                var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
-                                var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your registration is in Under Process. We will notify you once your Registration gets Complete. You can track your Registration Phase here.");
-                                for (var i = 0; i < 5; i++) {
-                                    if (studentPhase > i)
-                                        htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-accepted");
-                                    else
-                                        htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-process");
+                            try {
+                                var studentName = results[0].name;
+                                var studentPhase = results[0].registration_phase;
+                                var studentMainHtmlFile = htmlNewFile;
+                                if (studentPhase == 0) {
+                                    var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
+                                    var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your Application Gets Rejected.");
+                                    for (var i = 0; i < 5; i++)
+                                        htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-rejected")
+                                    res.send(htmlNewFile);
+                                } else {
+                                    var htmlNewFile = studentFile.replace("{%StudentName%}", studentName);
+                                    var htmlNewFile = htmlNewFile.replace("{%studentMessage%}", "your registration is in Under Process. We will notify you once your Registration gets Complete. You can track your Registration Phase here.");
+                                    for (var i = 0; i < 5; i++) {
+                                        if (studentPhase > i)
+                                            htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-accepted");
+                                        else
+                                            htmlNewFile = htmlNewFile.replace("{%trackClass%}", "main-track-process");
+                                    }
+                                    res.send(htmlNewFile);
                                 }
-                                res.send(htmlNewFile);
+                            } catch (e) {
+                                var str = "<a href='newUser' target='_blank'>Click here to register.</a>"
+                                res.send(str);
                             }
                         });
                     } else if (log === 'SUP') {
