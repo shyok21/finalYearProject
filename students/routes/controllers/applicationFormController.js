24e4ca07c849bb1a964055c30eafae2884f1b829
Abhishek150598 <abhishek150598@gmail.com>
2021-01-06 11:34:14 +0530

    Applicatin form DB connection

--- students/routes/controllers/applicationFormController.js
+++ students/routes/controllers/applicationFormController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const con = require('./../../db.js');
+const util = require('util');
 var htmlFile = fs.readFileSync("views/applicationForm.html", "utf-8");
 
 const applicationFormPage = (req, res) => {
@@ -7,8 +8,41 @@ const applicationFormPage = (req, res) => {
 }
 
 const applicationFormSubmit = (req, res) => { 
-    // Insert DB code here
-    res.send("Waiting for approval!");
+    const { student_name,
+            enrolment_number,
+            student_email,
+            dep_id,
+            address1,
+            address2,
+            supervisor_id,
+            cosupervisor_id,
+            title }  = req.body;
+
+    const address = address1 + ' ' + address2;
+    const registration_phase = 1;
+    var query = util.format(
+        "insert into student values('%s','%s','%s','%s','%s','%d','%s','%s','%s');", 
+        enrolment_number,
+        student_name,
+        student_email,
+        address,
+        title,
+        registration_phase,
+        supervisor_id,
+        cosupervisor_id,
+        dep_id
+    );
+    con.query(query, (err, results, fields) => {
+        if(err) {
+            console.log(err.message);
+        }
+        else {
+            console.log("Student Added to Student Database");
+            console.log(results);
+            res.send("<h1>Waiting for approval<h1>");
+        }
+        
+    });
 }
 
 module.exports = {
