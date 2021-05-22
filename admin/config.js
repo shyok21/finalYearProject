630fc00054beb7c83b9dc32474c68566f5b8acd5
shyok21 <shyokmutsuddi21@gmail.com>
2021-05-22 20:05:25 +0530

    some change

--- admin/config.js
+++ admin/config.js
@@ -8,7 +8,7 @@ const config = {
     DB_PORT: "3306",
     SENDER_EMAIL: 'ju.notifyserver@gmail.com',
     TEST_EMAIL: "ju.phdms2021@gmail.com",
-    TEST_MODE: true,
+    TEST_MODE: false,
     MAIL_SERVICE: 'gmail',
     CLIENT_ID : '284672905667-s38jh6jcjp5sepf9rtbs1hfghtuut1di.apps.googleusercontent.com',
     CLIENT_SECRET : 'cTb991ifri5Ss2B65NyPBWCJ',
