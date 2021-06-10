eba886de7a8f7e177a53ff9b4fe68881b19074de
shyok21 <shyokmutsuddi21@gmail.com>
2021-06-10 20:46:59 +0530

    new email

--- students/services/sendEmail.js
+++ students/services/sendEmail.js
@@ -1,58 +1,31 @@
+const { SENDER_EMAIL, SENDER_PASSWORD, TEST_EMAIL, TEST_MODE , MAIL_SERVICE } = require('./../config');
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const { SENDER_EMAIL, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN, TEST_EMAIL, TEST_MODE , MAIL_SERVICE } = require('./../config');
 
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-const getTransporter = async () => {
-    try {
-        const accessToken = await oAuth2Client.getAccessToken();
-
-        const transporter = nodemailer.createTransport({
-            service: MAIL_SERVICE,
-            auth: {
-                type: 'OAuth2',
-                user: SENDER_EMAIL,
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken,
-            }
-        });
-        return transporter;
+const transporter = nodemailer.createTransport({
+    pool: true,
+    port: 465,
+    host: "smtp.gmail.com",
+    secure: true,
+    tls: {
+        rejectUnauthorized: false
+    },
+    auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD
     }
-    catch(err) {
-        console.log(err);
-        throw err;
-    }
-}
-    
-const sendEmail = async (mailData, callback) => {
-    try {
-        const transporter = await getTransporter();
-        var mailOptions = {
-            from: SENDER_EMAIL,
-            to: TEST_MODE ? TEST_EMAIL : mailData.to,
-            subject: mailData.subject,
-            html: mailData.html
-        };
+});
+const sendEmail = (mailData, callback) => {
+    var mailOptions = {
+        from: SENDER_EMAIL,
+        to: TEST_MODE ? TEST_EMAIL : mailData.email,
+        subject: mailData.subject,
+        html: mailData.html
+    };
 
-        transporter.sendMail(mailOptions, function(error, info) {
-            if (typeof callback == "function")
-                callback(error, info);
-        });
-    }
-    catch(err) {
-        console.log(err);
+    transporter.sendMail(mailOptions, function(error, info) {
+        console.log(info);
         if (typeof callback == "function")
-            callback(err, null);
-    }
-    
+            callback(error, info);
+    });
 }
-
 module.exports = sendEmail;
---
+++ students/services/sendEmail2.js
@@ -0,0 +1,58 @@
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const { SENDER_EMAIL, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN, TEST_EMAIL, TEST_MODE , MAIL_SERVICE } = require('./../config');
+
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const getTransporter = async () => {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
+
+        const transporter = nodemailer.createTransport({
+            service: MAIL_SERVICE,
+            auth: {
+                type: 'OAuth2',
+                user: SENDER_EMAIL,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken,
+            }
+        });
+        return transporter;
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+}
+    
+const sendEmail = async (mailData, callback) => {
+    try {
+        const transporter = await getTransporter();
+        var mailOptions = {
+            from: SENDER_EMAIL,
+            to: TEST_MODE ? TEST_EMAIL : mailData.to,
+            subject: mailData.subject,
+            html: mailData.html
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (typeof callback == "function")
+                callback(error, info);
+        });
+    }
+    catch(err) {
+        console.log(err);
+        if (typeof callback == "function")
+            callback(err, null);
+    }
+    
+}
+
+module.exports = sendEmail;
